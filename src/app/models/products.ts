b948dd91526f9b644c761b0f20e22f5b0a7f61fb
Aurore  <hoshizora972@gmail.com>
2026-04-22 10:33:22 -0400

    Adding layout & routing

---
+++ src/app/models/products.ts
@@ -0,0 +1,12 @@
+// Mise en place de l'interface product  
+export type Product = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    rating: number;
+    reviewCount: number;
+    inStock: boolean;
+    category: string;
+}
